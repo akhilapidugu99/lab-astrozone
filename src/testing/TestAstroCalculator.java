f92db9bcef0995e42e76b311ad6de19d24547e65
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-09-23 15:08:34 +0530

    Update TestAstroCalculator.java

--- src/testing/TestAstroCalculator.java
+++ src/testing/TestAstroCalculator.java
@@ -11,19 +11,21 @@ public class TestAstroCalculator {
 	AstroCalculator astrocal = new AstroCalculator();
 	@Test
 	public void testFindSignMethod() {
-		User astro = new User("Clara","female","03-11-1997");
+		User astro = new User("Clara", "clara@gmail.com", "03-11-1997", "female");
 		assertEquals("Scorpio",astrocal.findSign(astro));
-		User astro1 = new User("Clara","female","04-04-1969");
+		User astro1 = new User("Clara", "clara@gmail.com", "04-04-1969", "female");
 		assertEquals("Aries",astrocal.findSign(astro1));
 		try {
-			User astro2 = new User(null,null,null);
+			User astro2 = new User(null,null,null, null);
 			astrocal.findSign(astro2);
-			User astro3 = new User(null,"female","03-11-1997");
+			User astro3 = new User(null,"clara@gmail.com","03-11-1997", "female");
 			astrocal.findSign(astro3);
-			User astro4 = new User("Clara",null,"03-11-1997");
+			User astro4 = new User("Clara",null,"03-11-1997", "female");
 			astrocal.findSign(astro4);
-			User astro5 = new User("Clara","female",null);
+			User astro5 = new User("Clara","clara@gmail.com", null, "female");
 			astrocal.findSign(astro5);
+                        User astro6 = new User("Clara","clara@gmail.com", "03-11-1997", null);
+			astrocal.findSign(astro6);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
