cc955ddb6257c881b7202d5ec8911de0cb9982a4
Akhila Pidugu <akhilapidugu99@gmail.com>
2021-01-20 21:49:28 +0530

    PROGRAD

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -2,6 +2,9 @@
 package controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,10 +12,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.User;
+import service.AstroCalculator;
+
 
 @WebServlet(urlPatterns = {"/entervalue"})
 public class AstroFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private int age;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/entervalues.jsp");
@@ -20,14 +27,26 @@ public class AstroFormController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/* 
+ 
 		String name=request.getParameter("name");
 		String gender=request.getParameter("gender");
 		String date=request.getParameter("dob1");
 		String month=request.getParameter("dob2");
 		String year=request.getParameter("dob3");
-		String dob = date+"-"+month+"-"+year;	
+		String dob = year+"-"+month+"-"+date;	
 		String email = request.getParameter("email");
+		LocalDate presentdate=LocalDate.now();
+		LocalDate userDate=LocalDate.of(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(date));
+		Period period=Period.between(userDate,presentdate);
+		age = period.getYears();
+		String astrosign = "";
+		User user=new User(name,email,dob,gender);
+		AstroCalculator ac=new AstroCalculator();
+		astrosign=ac.findSign(user);
+		System.out.println("Your details");
+		System.out.println("Name:"+name);
+		System.out.println("Age:"+age);
+		System.out.println("Astrology:"+astrosign);
 		if(astrosign.equals("Aquarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/aquarius.svg'>");
@@ -88,6 +107,6 @@ public class AstroFormController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
-		}*/
+		}
 	}
 }
